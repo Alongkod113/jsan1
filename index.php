84a1ad5e2303ec6e6c0350780ce4788587f1fdb2
Alongkod113 <80100652+Alongkod113@users.noreply.github.com>
2021-12-07 13:42:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <table>
         <thead>
             <tr>
-                <th>ID</th> <th>Title</th> <th>Author</th>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Author</th>
             </tr>
         </thead>
         <tbody id="tblPosts">
@@ -19,29 +23,32 @@
 </body>
 
 <script>
-    function loadePost(){
-        var url ="https://jsonplaceholder.typicode.com/posts";
+    function loadePost() {
+        var url = "https://jsonplaceholder.typicode.com/posts";
         $.getJSON(url)
-        .done((data)=>{
-            $.each(data,  (k, item)=>{
-                console.log(k);
-                console.log(item);
-                var line = "<tr>";
-                    line += "<td>" + (k+1) + "</td>";
-                    line += "<td>" + item.fname + "</td>";
-                    line += "<td>" + item.lname + "</td>";
-                    line += "</tr>";
-                $("#tblStudent").append(line);
+            .done((data) => {
+                $.each(data, (k, item) => {
+                    console.log(item);
+                    var line = "<tr>";
+                    line += "<td>" + item.id + "</td>";
+                    line += "<td><b>" + item.title + "<b><br/>";
+                    line +=  item.body + "</td>";
+                    line += "<td> link </td>";
+
+                    line +="</tr>";
+                    $("#tblStudent").append(line);
+                });
+            })
+            .fail((xhr, status, err) => {
             });
-        })
-        .fail((xhr, status, err)=>{
-        });
-}
-$(()=>{
-    loadJSON();
-});
-</script>
-</html>
     }
+    $(() => {
+        loadJSON();
+    });
 </script>
+
+</html>
+}
+</script>
+
 </html>
