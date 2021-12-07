9a68ce52283d3c59550459111d1695229572f866
Alongkod113 <80100652+Alongkod113@users.noreply.github.com>
2021-12-07 11:07:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alongkod</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
+<head>
+<body>
+    <button id="btnJSON1"> JSON 1</button>
+    Name : <span id="fname"></span> <br/>
+    Sname : <span id="lname"></span>
+</body>
+<script>
+function loadJSON(){
+    var url = "https://cj-android-demon.herokuapp.com/json1.php"
+    $.get(url,(data, status)=>{
+        console.log(data);
+        var jData = JSON.parse(data);
+        console.log(jData.fname);
+        console.log(jData.lname);
+        $("#name").text(jData.fname);
+    ));
+
+            
+}
+
+$(()=>{
+    $("#btnTSON1").click(loadJSON);
+
+});
+</script>
+</html>
